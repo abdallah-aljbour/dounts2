f91443afac58eb0b0e5f5443a5970401dfe69edc
abdallah-aljbour <abdallaaljbour03@gmail.com>
2024-06-04 17:01:46 +0300

    Dounts

---
+++ appp.js
@@ -0,0 +1,141 @@
+// Q1: comparison;
+
+let multiply = -5 * 3;
+console.log(multiply);
+let add = "JavaScript" + 50;
+console.log(add);
+let per = 17 % 5;
+console.log(per);
+let divide = 5 / 10;
+console.log(divide);
+let id = 4 === "4";
+console.log(id);
+let word = "Hello" + 5;
+console.log(word);
+let math = Math.ceil(4.5) - Math.floor(7.5);
+console.log(math);
+let power = Math.pow(5, 2);
+console.log(power);
+
+// Q2:inter a number in prompt and read in alert//
+
+// let number = prompt("inter a number");
+// alert(number);
+
+//Q3:Read in two numbers and display them in ascending order//
+// let first = prompt("inter first number");
+// let second = prompt("inter second number");
+// if (first > second) {
+//   console.log(first, second);
+// } else {
+//   console.log(second, first);
+// }
+
+//Q4: Read in two numbers and display the larger//
+
+// let larg1 = prompt("Type The First Number");
+// let larg2 = prompt("Type The Two Number");
+// if (larg1 > larg2) {
+//   alert(larg1);
+// } else {
+//   alert(larg2);
+// }
+
+//Q5:Read two numbers and display their sum. //
+// let sum1 = 10;
+// let sum2 = 20;
+// let sum = sum1 + sum2;
+// console.log(sum1 + sum2);
+
+// Q6: If you have a “Number” variable,  Your task is to print
+// let NUMBER = 1;
+// switch (NUMBER) {
+//   case 1:
+//     console.log("ONE");
+//     break;
+//   case 2:
+//     console.log("TWO");
+//     break;
+//   case 3:
+//     console.log("THREE");
+//     break;
+//   case 4:
+//     console.log("FOUR");
+//     break;
+//   case 5:
+//     console.log("FIVE");
+//     break;
+//   case 6:
+//     console.log("SEX");
+//     break;
+//   case 7:
+//     console.log("SEVEN,");
+//     break;
+//   case 8:
+//     console.log("EIGHT");
+//     break;
+//   case 9:
+//     console.log("NINE");
+//     break;
+//   default:
+//     console.log("PLEASE TRY AGAIN");
+// }
+
+//task 2
+// while (true) {
+//   let Gender = prompt("Your Gender", "Male Or Famale");
+//   if (Gender == "male") {
+//     alert("Mr" + Person);
+//     break;
+//   } else if (Gender == "female") {
+//     alert("Ms" + Person);
+//     break;
+//   }
+// }
+
+//task 2 b
+// let arr = [];
+// arr[0] = prompt("Enter Your Name", "Your Name");
+// arr[1] = prompt("Enter Your Gender", "Your Gener");
+// arr[2] = prompt("Enter TypeOf Order", "Dount , Ice Coffee ...");
+// arr[3] = prompt("Order Name");
+// alert(arr);
+
+//task 2 c
+// let data = 0;
+// let arr = [];
+// arr[0] = prompt("Enter Your Name", "Your Name");
+// arr[1] = prompt("Enter Your Gender", "Your Gener");
+// arr[2] = prompt("Enter TypeOf Order", "Dount , Ice Coffee ...");
+// arr[3] = prompt("Order Name");
+// for (i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// task 2 d
+
+// for (i = 0; i <= 5; i++) {
+//   alert(i);
+// }
+
+//task 2 E
+
+// let num = [];
+// for (let i = 0; i <= 5; i++) {
+//   // num += [i]; //num=num+i
+//   num.push(i);
+// }
+// alert(num);
+// let num = 0;
+// let arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i <= 5; i++) {
+//   num = arr[i]; //num=num+i
+// }
+// alert(num);
+
+//task 2 E
+// for (i = 0; i <= 20; i++) {
+//     if (i % 3 == 0) {
+//       alert(i);
+//     }
+//   }
